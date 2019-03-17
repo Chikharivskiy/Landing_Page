3f834cbc2e6f9e996ce8d67d460f014035828ed4
Maksim Chikharivskiy <maks9997777@gmail.com>
2019-03-17 15:03:42 +0200

    Header - complete

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var clickMenu = document.getElementById('button');
+    clickMenu.onclick = function(){
+        var clickMenu = document.getElementById('clickMenu');
+            clickMenu.classList.toggle('menu_active');
+    }
